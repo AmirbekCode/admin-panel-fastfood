c8643fc1a3d1ecb50a6a9830c315059582635b51
zairov01 <106994015+AmirbekCode@users.noreply.github.com>
2023-02-13 17:55:34 +0500

    Css Change

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,34 @@
-import formImg from "../../assets/images/IImg.png"
+import formImg from "../../assets/images/3390.png"
 
 
 function Login() {
     return (
-        <>
-        <div className="ImgBox">
-            <img src={formImg} alt="" />
-        </div>
-        <div className="form">
+        <div className="Login">
+            <div className="flexContent">
+                <div className="ImgBox">
+                    <img src={formImg} alt="" />
+                </div>
+                <div className="form">
+                    <h4 className="welcome">
+                        Tizimga xush kelibsiz !
+                    </h4>
+                    <span>Tizimga kirish uchun, login va parol orqali <br />
+                        autentifikatsiya jarayonidan o’ting</span>
+                    <div className="InputSection">
+                        <div className="Inputt">
+                            <div className="indicator1"></div>
+                            <input type="text" className="InputUser"/>
+                        </div>
+                        <div className="Inputt">
+                            <div className="indicator2"></div>
+                            <input type="password" className="InputPassword"/>                            
+                        </div>
+                    </div>
+                </div>
+                <button className="">Tizimga Kirish</button>
+            </div>
 
         </div>
-        </>
     )
 }
 
